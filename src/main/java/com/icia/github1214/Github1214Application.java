b5abe529cec037404970eb10d08a22056866a79c
yeoseungjae <dutmdwo98@gmail.com>
2021-12-14 18:33:20 +0900

    Intellij Github Test

---
+++ src/main/java/com/icia/github1214/Github1214Application.java
@@ -0,0 +1,13 @@
+package com.icia.github1214;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github1214Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github1214Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/icia/github1214/Github1214ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.github1214;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Github1214ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
